92ec155c9f61df2bf6cb5343764d9a5723d5d700
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-09-03 15:37:26 -0700

    basic room functionality: instance of room is Room, it knows its ID and basic rate

--- lib/room.rb
+++ lib/room.rb
@@ -1,2 +1,18 @@
 class Room
+ 
+ attr_reader :id, :cost
+ 
+ BASE_ROOM_COST = 200
+ 
+ def initialize(id: nil , cost: BASE_ROOM_COST)
+  @id = id
+  @cost = cost
+  
+  if @id == nil
+   raise ArgumentError.new("No ID number provided")
+  end
+  
+ end
+ 
+ 
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,12 +1,38 @@
 require_relative 'test_helper'
 
-xdescribe Room do
- 
- xit "instance of room knows its ID #" do
+describe Room do
+ before do
+  test_room_id = 5
+  test_room_basic_cost = 200
+  @test_room = Room.new(id: test_room_id, cost: test_room_basic_cost)
+  
+ end
+ it "room is of appropriate class" do
+  expect(@test_room).must_be_kind_of Room
  end
  
- xit "instance of room knows its basic cost" do
+ it "instance of room knows its ID #" do
+  expect(@test_room.id).must_equal 5
+ end
+ 
+ it "instance of room knows its basic cost" do
+  expect (@test_room.cost).must_equal 200
+ end
+end
+
+describe "Room initiation" do
+ it "verifies that the basic room rate for each room is $200" do
+  test_room_id = 5
+  @test_room = Room.new(id: test_room_id)
+  expect(@test_room.cost).must_equal 200
+ end
+ 
+ it "verifies that an ArgumentError is thrown if no room ID is used to instigate the instance of Room "do
+  begin
+  @test_room = Room.new
+  expect(@test_room.id).must_be_nil
+  rescue ArgumentError
+  end
  end
  
 end
-
