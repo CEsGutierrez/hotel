0612381af38fe8cd50ba25666c70642014b9754a
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-09-03 14:05:29 -0700

    Initial comit with largely empty files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,19 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "time"
+require "minitest/pride"
+require "simplecov"
+SimpleCov.start do 
+ add_filter 'test/' #Ignores coverage gap for tests
+end
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+require_relative '../lib/block'
+require_relative '../lib/booker'
+require_relative '../lib/date_mediator'
+require_relative '../lib/reporter'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
+
