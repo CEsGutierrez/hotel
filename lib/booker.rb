c70bda11b9eb007e6fe3aa79b4b476f27bb87b66
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-09-03 20:48:37 -0700

    booker populates totel with instances of Room

--- lib/booker.rb
+++ lib/booker.rb
@@ -1,2 +1,29 @@
+require 'Time'
+require_relative 'Room'
+require_relative 'Reservation'
+require_relative 'Reporter'
+
 class Booker
+ 
+ attr_accessor :rooms, :reservations
+ 
+ 
+ @reservations = []
+ 
+ def initialize 
+  @rooms = populate_hotel
+ end
+ 
+ def populate_hotel
+  rooms_array = []
+  i = 1
+  until i == 21 do 
+   rooms_array << Room.new(id: i)
+   i = i + 1
+  end
+  return rooms_array
+ end  
+ 
+ 
+ 
 end
--- test/booker_test.rb
+++ test/booker_test.rb
@@ -1,10 +1,26 @@
 require_relative 'test_helper'
 
-xdescribe Booker do
- xit "booker can instigate instances of blocks" do
+describe Booker do
+ before do
+  @booker = Booker.new
+ end
+ 
+ it "booker populates the hotel" do
+  expect(@booker.rooms.length).must_equal 20
+ end
+ 
+ it "booker hotel population is made up of rooms" do
+  arbitrary_test_room_id = 2
+  expect(@booker.rooms[arbitrary_test_room_id]).must_be_kind_of Room
  end
  
  xit "booker can instigate instances of reservation" do 
  end
  
+ xit "booker can instigate instances of blocks" do
+ end
+ 
+ 
+ 
 end
+
