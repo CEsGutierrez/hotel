07881e8bf82ee79f85147a6821be67ba1d46edc5
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-09-07 20:55:00 -0700

    Removed Block files and updated test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,10 +11,9 @@ require "rake"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative '../lib/block'
+
 require_relative '../lib/booker'
 require_relative '../lib/date_mediator'
 require_relative '../lib/reservation'
 require_relative '../lib/room'
 
-# require_relative '../lib/reporter'
